61b3857e04c7a6a827a74b42678490412abf8ce9
steffy037 <stephanie.ribou@gmail.com>
2020-04-02 10:49:39 +0200

    Initial commit

---
+++ src/pions/PositionDepart.java
@@ -0,0 +1,14 @@
+package pions;
+
+public class PositionDepart {
+
+    String [][] tab;
+
+    public PositionDepart(){
+        tab[0][0] = "@../image/PN.gif";
+        tab[1][1]="@../image/TN.gif";
+    }
+
+
+
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import pions.PositionDepart;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+    private PositionDepart positionDepart;
+
+    //@FXML
+    //private Im textFieldVisuel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
