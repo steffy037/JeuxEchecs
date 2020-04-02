089a32d26e0a076249bb44e53d24bd3d65ca4e1b
steffy037 <stephanie.ribou@gmail.com>
2020-04-02 18:13:26 +0200

    récupération des coordonnées des panes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,23 +1,47 @@
 package sample;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    private Pane [][] panes;
+
     @FXML
     private GridPane gridPane;
+
+    @FXML
+    private Pane pane1, pane2, pane3, pane4, pane5, pane6, pane7, pane8;
+
+    @FXML
+    private Pane pane9, pane10, pane11, pane12,pane13,pane14, pane15, pane16;
+
+    @FXML
+    private Pane pane17, pane18, pane19, pane20, pane21, pane22, pane23, pane24;
+
+    @FXML
+    private Pane pane25, pane26, pane27, pane28,pane29,pane30, pane31, pane32;
+
+    @FXML
+    private Pane pane33, pane34, pane35, pane36, pane37, pane38, pane39, pane40;
+
+    @FXML
+    private Pane pane41, pane42, pane43, pane44,pane45,pane46, pane47, pane48;
+
+    @FXML
+    private Pane pane49, pane50, pane51, pane52, pane53, pane54, pane55, pane56;
+
+    @FXML
+    private Pane pane57, pane58, pane59, pane60,pane61,pane62, pane63, pane64;
+
 
     ImageView tourNoir = new ImageView(new Image("image/TN.gif"));
     ImageView cavalierNoir = new ImageView(new Image("image/CN.gif"));
@@ -58,59 +82,89 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        panes = new Pane[8][8];
+
+        Pane [] ligne0 = {pane1,pane2,pane3,pane4,pane5, pane6, pane7,pane8};
+        Pane [] ligne1 = {pane9,pane10,pane11,pane12,pane13, pane14, pane15,pane16};
+        Pane [] ligne2 = {pane17,pane18,pane19,pane20,pane21, pane22, pane23,pane24};
+        Pane [] ligne3 = {pane25,pane26,pane27,pane28,pane29, pane30, pane31,pane32};
+        Pane [] ligne4 = {pane33,pane34,pane35,pane36,pane37, pane38, pane39,pane40};
+        Pane [] ligne5 = {pane41,pane42,pane43,pane44,pane45, pane46, pane47,pane48};
+        Pane [] ligne6 = {pane49,pane50,pane51,pane52,pane53, pane54, pane55,pane56};
+        Pane [] ligne7 = {pane57,pane58,pane59,pane60,pane61, pane62, pane63,pane64};
+
+        panes[0] = ligne0;
+        panes[1] = ligne1;
+        panes[2] = ligne2;
+        panes[3] = ligne3;
+        panes[4] = ligne4;
+        panes[5] = ligne5;
+        panes[6] = ligne6;
+        panes[7] = ligne7;
+
+
         positionDepart();
-
-
 
 
     }
 
 
+    public void paneClic(MouseEvent event){
+
+        Pane paneClic = (Pane)event.getSource();
+        for(int i = 0; i < panes.length; i++)
+        {
+            for(int j = 0; j < panes.length; j++)
+            {
+                if(panes[i][j]==paneClic){
+                    System.out.print("x : " + i + " y : " + j);
+                }
+            }
+        }
+
+    }
+
+
+
     public void positionDepart(){
 
+        pane1.getChildren().add(tourNoir);
+        pane2.getChildren().add(cavalierNoir);
+        pane3.getChildren().add(fouNoir);
+        pane4.getChildren().add(roiNoir);
+        pane5.getChildren().add(renneNoir);
+        pane6.getChildren().add(fouNoir2);
+        pane7.getChildren().add(cavalierNoir2);
+        pane8.getChildren().add(tourNoir2);
 
-        gridPane.add(tourNoir, 0, 0);
-        gridPane.add(cavalierNoir, 1, 0);
-        gridPane.add(fouNoir, 2, 0);
-        gridPane.add(roiNoir, 3, 0);
-        gridPane.add(renneNoir, 4, 0);
-        gridPane.add(fouNoir2, 5, 0);
-        gridPane.add(cavalierNoir2, 6, 0);
-        gridPane.add(tourNoir2, 7, 0);
+        pane9.getChildren().add(pionNoir);
+        pane10.getChildren().add(pionNoir1);
+        pane11.getChildren().add(pionNoir2);
+        pane12.getChildren().add(pionNoir3);
+        pane13.getChildren().add(pionNoir4);
+        pane14.getChildren().add(pionNoir5);
+        pane15.getChildren().add(pionNoir6);
+        pane16.getChildren().add(pionNoir7);
+
+        pane49.getChildren().add(pionBlanc);
+        pane50.getChildren().add(pionBlanc1);
+        pane51.getChildren().add(pionBlanc2);
+        pane52.getChildren().add(pionBlanc3);
+        pane53.getChildren().add(pionBlanc4);
+        pane54.getChildren().add(pionBlanc5);
+        pane55.getChildren().add(pionBlanc6);
+        pane56.getChildren().add(pionBlanc7);
+
+        pane57.getChildren().add(tourBlanc);
+        pane58.getChildren().add(cavalierBlanc);
+        pane59.getChildren().add(fouBlanc);
+        pane60.getChildren().add(roiBlanc);
+        pane61.getChildren().add(renneBlanc);
+        pane62.getChildren().add(fouBlanc2);
+        pane63.getChildren().add(cavalierBlanc2);
+        pane64.getChildren().add(tourBlanc2);
 
 
-
-
-        gridPane.add(tourBlanc, 0, 7);
-        gridPane.add(cavalierBlanc, 1, 7);
-        gridPane.add(fouBlanc, 2, 7);
-        gridPane.add(roiBlanc, 3, 7);
-        gridPane.add(renneBlanc, 4, 7);
-        gridPane.add(fouBlanc2, 5, 7);
-        gridPane.add(cavalierBlanc2, 6, 7);
-        gridPane.add(tourBlanc2, 7, 7);
-
-
-
-        gridPane.add(pionBlanc, 0, 6);
-        gridPane.add(pionBlanc1, 1, 6);
-        gridPane.add(pionBlanc2, 2, 6);
-        gridPane.add(pionBlanc3, 3, 6);
-        gridPane.add(pionBlanc4, 4, 6);
-        gridPane.add(pionBlanc5, 5, 6);
-        gridPane.add(pionBlanc6, 6, 6);
-        gridPane.add(pionBlanc7, 7, 6);
-
-
-
-        gridPane.add(pionNoir, 0, 1);
-        gridPane.add(pionNoir1, 1, 1);
-        gridPane.add(pionNoir2, 2, 1);
-        gridPane.add(pionNoir3, 3, 1);
-        gridPane.add(pionNoir4, 4, 1);
-        gridPane.add(pionNoir5, 5, 1);
-        gridPane.add(pionNoir6, 6, 1);
-        gridPane.add(pionNoir7, 7, 1);
 
     }
 }
